f8ff7087fe306bb980624c4060b2e26653ace2dd
MahithaAmara <mahitha.v.amara@gmail.com>
2024-11-04 19:28:58 +0530

    testcommit

--- accounts/models.py
+++ accounts/models.py
@@ -1,3 +1,6 @@
 from django.db import models
 
-# Create your models here.
+# Create your models here
+class Accounts(models.Model):
+    cname=models.CharField(max_length=100)
+    date=models.DateField()
--- products/models.py
+++ products/models.py
@@ -1,3 +1,5 @@
 from django.db import models
 
-# Create your models here.
+class products(models.Model):
+    silver=models.IntegerField()
+    gold=models.IntegerField()
