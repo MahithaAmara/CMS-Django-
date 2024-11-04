08a73261fa6524195b7e6c073358efd395a8646c
MahithaAmara <mahitha.v.amara@gmail.com>
2024-11-04 21:31:54 +0530

    ORM views

--- accounts/views.py
+++ accounts/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-
+from accounts.models import *
 # Create your views here.
 def register(request):
     return HttpResponse("Registration for Accounts") 
@@ -10,4 +10,24 @@ def update(request):
 
 def delete(request):
     return HttpResponse("delete for Accounts") 
+
+#ORM views
+def register(request):
+    if request.method == "POST":
+        cname = request.POST.get('cname')
+        date = request.POST.get('date')
+        pno=request.POST.get('pno')
+        accounts.objects.create(cname=cname,date=date,pno=pno)
+        queryset = accounts.objects.all()
+
+def update(request):
+    if request.method == "POST":
+        data=request.POST()
+        cname = data.get('cname')
+        date = data.get('date')
+        pno = data.get('pno')
+
+def delete(request,date):
+    queryset = accounts.objects.get(date=date)
+    queryset.delete()
 
